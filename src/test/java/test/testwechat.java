c78f066729d6566cc1722bb62a4de0e9f5fe2d56
GrapeSoftCN <jk282@163.com>
2017-05-20 18:58:49 +0800

    11

---
+++ src/test/java/test/testwechat.java
@@ -0,0 +1,18 @@
+package test;
+
+import httpServer.booter;
+import interfaceApplication.Report;
+
+public class testwechat {
+	public static void main(String[] args) {
+		 booter booter = new booter();
+		 try {
+		 System.out.println("GrapeReport!");
+		 System.setProperty("AppName", "GrapeReport");
+		 booter.start(1003);
+		} catch (Exception e) {
+		}
+//		String string = "{\"name\":\"test\",\"phone\":\"13356897845\",\"IDCard\":\"340721199211262414\",\"openid\":\"oZU2Lw7s_7bATZXXJL5L2CvmFrCY\"}";
+//		System.out.println(new Report().Certification(string));
+	}
+}
